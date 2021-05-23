5330535b2c2d3f577f4ff9e817cd9655f72a4471
yamenrbd <yamenrbd@hotmail.com>
2021-05-23 12:54:31 +0300

    add auth app and create login page

--- config/urls.py
+++ config/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import contrib
+from django import urls
 from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+    # User managment this includes a lot of pages for account managemnt 
+    # can be seen in official auth doc
+    path('accounts/', include('django.contrib.auth.urls')),
+    # local apps
     path('', include('main_app.urls')),
     
 ]
